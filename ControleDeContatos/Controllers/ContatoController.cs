905f139d285b3124581cace830838d5b9f063e87
rhaneyko <rhaneykohonorio@gmail.com>
2024-05-27 21:55:30 -0400

    commit

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -8,5 +8,17 @@ namespace ControleDeContatos.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
     }
 }
